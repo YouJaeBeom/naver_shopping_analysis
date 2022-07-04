2035b2eecbc75ca72a210016b2fbd399cc6b55b9
root <dbwo4011@gmail.com>
2022-07-04 14:07:16 +0900

    2022-07-04 multi processing update

--- Products_Tracking_Page/products_rank_analysis.py
+++ Products_Tracking_Page/products_rank_analysis.py
@@ -1,66 +1,78 @@
-from tkinter.messagebox import NO
+#from tkinter.messagebox import NO
 import requests
 import json
 import detail_information_analysis
 from bs4 import BeautifulSoup as bs
 from datetime import datetime
+import multiprocessing.pool
+from itertools import repeat
 
-def ranking_analysis_PC(keyword):
-    for pagingIndex in range(1,51):
-        params = {
-            'sort': 'rel',
-            'pagingIndex': pagingIndex,
-            'pagingSize': '80',
-            'viewType': 'list',
-            'productSet': 'total',
-            'frm': 'NVSHTTL',
-            'query': keyword,
-            'origQuery': keyword,
-        }
+def ranking_analysis_PC(keyword, pagingIndex):
+    params = {
+        'sort': 'rel',
+        'pagingIndex': pagingIndex,
+        'pagingSize': '80',
+        'viewType': 'list',
+        'productSet': 'total',
+        'frm': 'NVSHTTL',
+        'query': keyword,
+        'origQuery': keyword,
+    }
 
-        response = requests.get('https://search.shopping.naver.com/api/search/all', params=params)
-        response_json = json.loads(response.text)
-        data_list =  response_json['shoppingResult']['products']
+    response = requests.get('https://search.shopping.naver.com/api/search/all', params=params)
+    response_json = json.loads(response.text)
+    data_list =  response_json['shoppingResult']['products']
 
-        for data in data_list:
-            rank = data['rank']
-            imageUrl = data['imageUrl']
-            productName = data['productName']
+    for data in data_list:
+        rank = data['rank']
+        imageUrl = data['imageUrl']
+        productName = data['productName']
 
-            ## category_n_id 
-            category1Id = data['category1Id']
-            category2Id = data['category2Id']
-            category3Id = data['category3Id']
-            category4Id = data['category4Id']
+        ## category_n_id 
+        category1Id = data['category1Id']
+        category2Id = data['category2Id']
+        category3Id = data['category3Id']
+        category4Id = data['category4Id']
 
-            ## category_n_name 
-            category1Name = data['category1Name']
-            category2Name = data['category2Name']
-            category3Name = data['category3Name']
-            category4Name = data['category4Name']
+        ## category_n_name 
+        category1Name = data['category1Name']
+        category2Name = data['category2Name']
+        category3Name = data['category3Name']
+        category4Name = data['category4Name']
 
-            mallName = data['mallName']
-            try:
-                mallGrade = data['mallInfoCache']['mallGrade']
-            except:
-                mallGrade = None
-            price = data['price']
-            reviewCount = data['reviewCount']
-            purchaseCnt = data['purchaseCnt']
-            keepCnt = data['keepCnt']
-            openDate = data['openDate']
-            score = data['scoreInfo']
+        mallName = data['mallName']
+        try:
+            mallGrade = data['mallInfoCache']['mallGrade']
+        except:
+            mallGrade = None
+        price = data['price']
+        reviewCount = data['reviewCount']
+        purchaseCnt = data['purchaseCnt']
+        keepCnt = data['keepCnt']
+        openDate = data['openDate']
+        score = data['scoreInfo']
 
-            mallProductId = data['mallProductId']
-            if mallProductId != "":
-                store_keepCnt, qnaCnt, related_tag= detail_information_analysis.getinfo(mallProductId)
-            else :
-                store_keepCnt, qnaCnt, related_tag = None, None, None
-            print( "{} rank = {} productName = {} price= {} reviewCount= {} purchaseCnt= {} keepCnt= {} store_keepCnt= {} score= {} qnaCnt= {} ".format(pagingIndex, rank, productName, price, reviewCount, purchaseCnt, keepCnt, store_keepCnt, score, qnaCnt))
-            #print("related_tag : ",related_tag)
+        mallProductId = data['mallProductId']
+        if mallProductId != "":
+            store_keepCnt, qnaCnt, related_tag= detail_information_analysis.getinfo(mallProductId)
+        else :
+            store_keepCnt, qnaCnt, related_tag = None, None, None
+        print( "{} rank = {} productName = {} price= {} reviewCount= {} purchaseCnt= {} keepCnt= {} store_keepCnt= {} score= {} qnaCnt= {} ".format(pagingIndex, rank, productName, price, reviewCount, purchaseCnt, keepCnt, store_keepCnt, score, qnaCnt))
+        #print("related_tag : ",related_tag)
 
 if __name__ == "__main__":
     starttime = datetime.now()
-    (ranking_analysis_PC("마스크"))
+    keyword = "마스크"
+
+    pageCnt = 50
+    pageCnt_list = []
+    for index in range(1,pageCnt):
+        pageCnt_list.append(index)
+
+    process_pool = multiprocessing.Pool(processes = pageCnt)
+    process_pool.starmap(ranking_analysis_PC, zip(repeat(keyword), pageCnt_list))
+    process_pool.close()
+    process_pool.join()
+
     endtime = datetime.now()
     print("time : ", endtime-starttime)
--- Products_Tracking_Page/products_rank_tracking.py
+++ Products_Tracking_Page/products_rank_tracking.py
@@ -3,84 +3,98 @@ import requests
 import json
 import detail_information_tracking
 from bs4 import BeautifulSoup as bs
+import multiprocessing.pool
+from itertools import repeat
 
-def ranking_tracking_PC(keyword):
-    for pagingIndex in range(1,51):
-        params = {
-            'sort': 'rel',
-            'pagingIndex': str(pagingIndex),
-            'pagingSize': '80',
-            'viewType': 'list',
-            'productSet': 'checkout',
-            'frm': 'NVSHCHK',
-            'query': keyword,
-            'origQuery': keyword,
-        }
+def ranking_tracking_PC(keyword, pagingIndex):
+    #for pagingIndex in range(1,51):
+    params = {
+        'sort': 'rel',
+        'pagingIndex': str(pagingIndex),
+        'pagingSize': '80',
+        'viewType': 'list',
+        'productSet': 'checkout',
+        'frm': 'NVSHCHK',
+        'query': keyword,
+        'origQuery': keyword,
+    }
 
-        response = requests.get('https://search.shopping.naver.com/api/search/all', params=params)
-        response_json = json.loads(response.text)
-        data_list =  response_json['shoppingResult']['products']
+    response = requests.get('https://search.shopping.naver.com/api/search/all', params=params)
+    response_json = json.loads(response.text)
+    data_list =  response_json['shoppingResult']['products']
 
-        for data in data_list:
-            product_id = data['id']
-            rank = data['rank']
-            imageUrl = data['imageUrl']
-            productName = data['productName']
-            price = data['price']
-            reviewCount = data['reviewCount']
-            purchaseCnt = data['purchaseCnt']
-            keepCnt = data['keepCnt']
-            score = data['scoreInfo']
+    for data in data_list:
+        product_id = data['id']
+        rank = data['rank']
+        imageUrl = data['imageUrl']
+        productName = data['productName']
+        price = data['price']
+        reviewCount = data['reviewCount']
+        purchaseCnt = data['purchaseCnt']
+        keepCnt = data['keepCnt']
+        score = data['scoreInfo']
 
-            mallProductId = data['mallProductId']
-            if mallProductId != "":
-                store_keepCnt, qnaCnt = detail_information_tracking.getinfo(mallProductId)
-            else :
-                store_keepCnt, qnaCnt = None, None
+        mallProductId = data['mallProductId']
+        if mallProductId != "":
+            store_keepCnt, qnaCnt = detail_information_tracking.getinfo(mallProductId)
+        else :
+            store_keepCnt, qnaCnt = None, None
 
-            print( "{} rank = {} productName = {} price= {} reviewCount= {} purchaseCnt= {} keepCnt= {} store_keepCnt= {} score= {} qnaCnt= {} ".format(pagingIndex, rank, productName, price, reviewCount, purchaseCnt, keepCnt, store_keepCnt, score, qnaCnt))
+        print( "{} rank = {} productName = {} price= {} reviewCount= {} purchaseCnt= {} keepCnt= {} store_keepCnt= {} score= {} qnaCnt= {} ".format(pagingIndex, rank, productName, price, reviewCount, purchaseCnt, keepCnt, store_keepCnt, score, qnaCnt))
 
-def ranking_tracking_MOB(keyword):
-    for pagingIndex in range(1,51):
-        headers = {
-            'Referer': 'https://msearch.shopping.naver.com/search/all?query={}&frm=NVSHSRC&prevQuery={}'.format(keyword,keyword).encode('utf-8').decode('iso-8859-1'),
-        }
+def ranking_tracking_MOB(keyword, pagingIndex):
+    headers = {
+        'Referer': 'https://msearch.shopping.naver.com/search/all?query={}&frm=NVSHSRC&prevQuery={}'.format(keyword,keyword).encode('utf-8').decode('iso-8859-1'),
+    }
 
-        params = {
-            'query': keyword,
-            'sort': 'rel',
-            'pagingIndex': str(pagingIndex),
-            'pagingSize': '80',
-            'viewType': 'lst',
-            'productSet': 'total',
-            'frm': 'NVSHPAG',
-            'origQuery': keyword,
-        }
+    params = {
+        'query': keyword,
+        'sort': 'rel',
+        'pagingIndex': str(pagingIndex),
+        'pagingSize': '80',
+        'viewType': 'lst',
+        'productSet': 'total',
+        'frm': 'NVSHPAG',
+        'origQuery': keyword,
+    }
+    
+    response = requests.get('https://msearch.shopping.naver.com/api/search/all', params=params, headers=headers)
+    response_json = json.loads(response.text)
+    data_list =  response_json['shoppingResult']['products']
+
+    for data in data_list:
+        product_id = data['id']
+        rank = data['rank']
+        imageUrl = data['imageUrl']
+        productName = data['productName']
+        price = data['price']
+        reviewCount = data['reviewCount']
+        purchaseCnt = data['purchaseCnt']
+        keepCnt = data['keepCnt']
+        score = data['scoreInfo']
         
-        response = requests.get('https://msearch.shopping.naver.com/api/search/all', params=params, headers=headers)
-        response_json = json.loads(response.text)
-        data_list =  response_json['shoppingResult']['products']
-
-        for data in data_list:
-            product_id = data['id']
-            rank = data['rank']
-            imageUrl = data['imageUrl']
-            productName = data['productName']
-            price = data['price']
-            reviewCount = data['reviewCount']
-            purchaseCnt = data['purchaseCnt']
-            keepCnt = data['keepCnt']
-            score = data['scoreInfo']
-            
-            mallProductId = data['mallProductId']
-            if mallProductId != "":
-                store_keepCnt, qnaCnt = detail_information_tracking.getinfo(mallProductId)
-            else :
-                store_keepCnt, qnaCnt = None, None
-            print( "{} rank = {} productName = {} price= {} reviewCount= {} purchaseCnt= {} keepCnt= {} store_keepCnt= {} score= {} qnaCnt= {} ".format(mallProductId, rank, productName, price, reviewCount, purchaseCnt, keepCnt, store_keepCnt, score, qnaCnt))
+        mallProductId = data['mallProductId']
+        if mallProductId != "":
+            store_keepCnt, qnaCnt = detail_information_tracking.getinfo(mallProductId)
+        else :
+            store_keepCnt, qnaCnt = None, None
+        print( "{} rank = {} productName = {} price= {} reviewCount= {} purchaseCnt= {} keepCnt= {} store_keepCnt= {} score= {} qnaCnt= {} ".format(mallProductId, rank, productName, price, reviewCount, purchaseCnt, keepCnt, store_keepCnt, score, qnaCnt))
 
 if __name__ == "__main__":
     starttime = datetime.now()
-    (ranking_tracking_PC("마스크"))
+    keyword = "마스크"
+    #(ranking_tracking_PC("마스크"))
+    
+    
+    pageCnt = 50
+    pageCnt_list = []
+    for index in range(1,pageCnt):
+        pageCnt_list.append(index)
+
+    process_pool = multiprocessing.Pool(processes = pageCnt)
+    process_pool.starmap(ranking_tracking_PC, zip(repeat(keyword), pageCnt_list))
+    process_pool.close()
+    process_pool.join()
+
     endtime = datetime.now()
     print("time : ", endtime-starttime)
